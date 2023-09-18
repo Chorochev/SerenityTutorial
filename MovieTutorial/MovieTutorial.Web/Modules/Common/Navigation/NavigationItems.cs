d7c37fa74c5dfb8433a4bf0d68df84ee8dbe476a
chorochev <chorochev@ya.ru>
2023-09-18 14:42:35 +0600

    Adding Several Genre Definitions

--- MovieTutorial/MovieTutorial.Web/Modules/Common/Navigation/NavigationItems.cs
+++ MovieTutorial/MovieTutorial.Web/Modules/Common/Navigation/NavigationItems.cs
@@ -1,7 +1,8 @@
 using Serenity.Navigation;
-using MyPages = MovieTutorial.MovieDB.Pages;
+using MovieDB = MovieTutorial.MovieDB.Pages;
 
 [assembly: NavigationLink(1000, "Dashboard", url: "~/", permission: "", icon: "fa-tachometer")]
 
 [assembly: NavigationMenu(2000, "Movie Database", icon: "fa-film")]
-[assembly: NavigationLink(2100, "Movie Database/Movies", typeof(MyPages.MoviePage), icon: "fa-video-camera")]
+[assembly: NavigationLink(2100, "Movie Database/Movies", typeof(MovieDB.MoviePage), icon: "fa-video-camera")]
+[assembly: NavigationLink(2200, "Movie Database/Genres", typeof(MovieDB.GenrePage), icon: "fa-thumb-tack")]

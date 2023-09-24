e3d4f44927684cd8982b914b95af366b80522cc3
chorochev <chorochev@ya.ru>
2023-09-24 22:03:20 +0600

    Getting data from a rest service

--- ReactSerene/ReactSerene.Web/wwwroot/Scripts/books/index.jsx
+++ ReactSerene/ReactSerene.Web/wwwroot/Scripts/books/index.jsx
@@ -1,14 +1,44 @@
-class CommentBox extends React.Component {
+class BooksList extends React.Component {
     render() {
-        return React.createElement(
-            'div',
-            { className: 'commentBox' },
-            'Hello, world! I am a CommentBox.',
+        const booksNodes = this.props.data.map(book =>
+        (<div key={book.id}>
+            UserId={book.userId};&nbsp;Title={book.title};
+        </div>))
+        return <div className="BooksList">{booksNodes}</div>;
+    }
+}
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { data: [] };
+        this.loadBooksFromRestService();
+    }
+
+    loadBooksFromRestService() {
+        console.log("call => loadBooksFromRestService")        
+        const restBooksUrl = "https://jsonplaceholder.typicode.com/todos/"
+        const xhr = new XMLHttpRequest();
+        xhr.open('get', restBooksUrl, true);
+        xhr.onload = () => {
+            const data = JSON.parse(xhr.response);
+            console.log(data);
+            this.setState({ data: data });
+        };
+        xhr.send();
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <BooksList key="booksListId" data={this.state.data} loadbooks={this.loadBooksFromRestService} />
+            </div>
         );
     }
 }
 
 ReactDOM.render(
-    React.createElement(CommentBox, null),
+    React.createElement(App, null),
     document.getElementById('bookscontent'),
 );

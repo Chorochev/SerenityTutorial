8c9cae4696e280b09324ac7119382092b976e529
chorochev <chorochev@ya.ru>
2023-09-24 13:53:50 +0600

    Add a simple REACT page

--- RESTSerene/RESTSerene.Web/Modules/Common/Navigation/NavigationItems.cs
+++ RESTSerene/RESTSerene.Web/Modules/Common/Navigation/NavigationItems.cs
@@ -2,3 +2,4 @@ using Serenity.Navigation;
 
 [assembly: NavigationLink(1000, "Dashboard", url: "~/", permission: "", icon: "fa-tachometer")]
 [assembly: NavigationLink(2000, "Books", url: "~/Books", permission: "", icon: "fa-book")]
+[assembly: NavigationLink(3000, "REACTPage", url: "~/REACTPage", permission: "", icon: "fa-react")]
